c81408c04b326064b9f6b59353cac6606a394363
Gabriela Santos <gabriela.pereira.santos@usp.br>
2020-12-09 12:05:57 -0300

    tipo usuario
    tipo usuario

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -2,20 +2,48 @@ const { ApolloServer, gql } = require('apollo-server')
 
 // gql - taget template
 const typeDefs = gql`
+    scalar Date # definiição de um tipo
+
     # Pontos de entradas da API
+    type Usuario {
+        id: ID!
+        nome: String!
+        email: String!
+        idade: Int 
+        salario: Float
+        vip: Boolean
+    }
+    
     type Query {
-        ola: String # consulta que retorna string
-        horaAtual: String
+        ola: String! # consulta que retorna string
+        horaAtual: Date!
+        usuarioLogado: Usuario
     }
 `
 // função que recebe parametros
 const resolvers = {
+    // consultas
+    Usuario: {
+        salario(usuario) {
+            return usuario.salario_real
+        }
+    },
     Query: {
         ola() {
             return 'Ola mundo'
         },
         horaAtual() {
-            return `${new Date}`
+            return new Date
+        },
+        usuarioLogado(){
+            return {
+                id: 1, 
+                nome: 'Gabriela Santos', 
+                email: 'gabriela@teste.com', 
+                idade: 24,
+                salario_real: 100000.50,
+                vip: true,
+            }
         }
     }
 }
